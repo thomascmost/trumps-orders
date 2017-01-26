551d038cb6da5e34167be4f8365ea8a8a86cbbba
Thomas C. Moore <tomismore@gmail.com>
2017-01-25 23:04:10 -0500

    nvm. got it

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,11 +8,13 @@ module Route {
 
     public static index(req: express.Request, res: express.Response, next: express.NextFunction) {
       //render page
+      console.log('found server')
         res.sendFile('index.html', {root: __dirname + '../public/'}) //Our main application, including client.js, is hooked into index.html
     }
     public static page(req: express.Request, res: express.Response, next: express.NextFunction) {
       //render page
-        res.sendFile('index.html', {root: __dirname + '../../public/'}) //Our main application, including client.js, is hooked into index.html
+      console.log('found page')
+        res.sendFile('index.html', {root: __dirname + '/../../../public/'}) //Our main application, including client.js, is hooked into index.html
     }
   }
 }
