38f7e75b6ea8b29dc6123ef1b5a3dca3f2b6bd1e
Thomas C. Moore <tomismore@gmail.com>
2017-01-25 20:08:27 -0500

    Got orders working

--- src/client/trump-order.component.ts
+++ src/client/trump-order.component.ts
@@ -3,18 +3,26 @@ require('./sass/index');
 import 'zone.js';
 import 'reflect-metadata';
 import { Component, Input } from '@angular/core';
-import Order from './order.class';
+import { Order } from './order';
 
 @Component({
-  selector: 'trumps-order',
+  selector: 'trump-order',
   template: `
-  <div class="trump-order">
-     <img class="trump-order-img" src="img/main.png"/>
-     <h4>{{mame}}</h4>
-     <p>{{description}}</p>
-  </div>`
+   <div class="trump-order">
+      <img class="trump-order-img" src="img/main.png"/>
+
+      <div class="number">
+         {{index+1}}
+      </div>
+      <div class="contents">
+         <h4 class="name">{{order.name}}</h4>
+         <h6 class="description">{{order.description}}</h6>
+      </div>
+   </div>`
 })
 
 export class TrumpOrderComponent {
-  @Input() order: Order;
+   @Input() index: number;
+   @Input() order: Order;
+}
 
