4778f7c18c5139382669151678a830483144b44b
Thomas C. Moore <tomismore@gmail.com>
2017-01-25 22:39:21 -0500

    attempt at solving routing issues

--- src/server/app.ts
+++ src/server/app.ts
@@ -22,8 +22,11 @@ var env = process.env.NODE_ENV || 'development';
 
 // Routes
 
+app.get('/sources', routes.Index.index);
+app.get('/disposition', routes.Index.index);
+app.get('/*', routes.Index.index);
 app.get('/', routes.Index.index);
-app.get('/:pageCalled', routes.Index.index);
+app.get('*', routes.Index.index);
 
 app.listen(app.get('port'), function() {
   console.log('app is running on port', app.get('port'));
